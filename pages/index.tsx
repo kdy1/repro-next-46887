796be9d3524932a8086412a7a824bc0b90d0a5b0
Daan Boer <daan-boer@hotmail.com>
2023-03-07 16:55:25 +0100

    Add code for reproduction example
    Issuing
    ```
    pnpm build
    pnpm start
    ```
    and visiting `localhost:3000` will result in a client-side exception.
    
    However, when setting `swcMinify: false`, the page will function
    just fine.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,18 @@
-/** Add your relevant code here for the issue to reproduce */
+// @ts-ignore
+import { Cite } from "@citation-js/core";
+import "@citation-js/plugin-csl";
+
+const reference = { id: 'a', title: 'Item A', issued: { 'date-parts': [[2016]] } };
+
+export function formatReference(r: any): string {
+  const cite = new Cite(r);
+
+  const bib = cite.format("bibliography", {format: "text", template: "apa"});
+  console.log(bib);
+
+  return bib;
+}
+
 export default function Home() {
-  return null
+  return <text>{formatReference(reference)}</text>
 }
